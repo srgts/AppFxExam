76a7c9c476fe0222c4ad6c07e50adf31d04e712e
Tsaturov Sergey <tsaturovsg@gmail.com>
2018-02-11 20:37:52 +0300

    Поработал4

--- exam1/src/main/java/com/sample/controllers/MainController.java
+++ exam1/src/main/java/com/sample/controllers/MainController.java
@@ -5,60 +5,67 @@ import com.sample.objects.Note;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class MainController {
+import static javafx.stage.Modality.WINDOW_MODAL;
 
+public class MainController {
     private DBNotesTable db = new DBNotesTable();
+    private Stage mainStage;
 
     @FXML
     private TableView tableNotesTable;
+    @FXML
+    private TableColumn<Note, String> columnDate;
+    @FXML
+    private TableColumn<Note, String> columnText;
+
+    private Parent fxmlAdd;
+    private FXMLLoader fxmlLoader = new FXMLLoader();
+    private AddDialogController addDialogController;
+    private Stage addDialogStage;
 
     @FXML
-    private TableColumn <Note, String> columnNumber;
-
-    @FXML
-    private TableColumn <Note, String> columnDate;
-
-    @FXML
-    private TableColumn <Note, String> columnText;
-
-//    private AddDialogController addDialogController;
-
-    @FXML
-    private void initialize(){
-        columnNumber.setCellValueFactory(new PropertyValueFactory<Note, String>("number"));
+    private void initialize() {
         columnDate.setCellValueFactory(new PropertyValueFactory<Note, String>("date"));
         columnText.setCellValueFactory(new PropertyValueFactory<Note, String>("text"));
-
         db.getUpdateTable();
         tableNotesTable.setItems(db.getNotes());
 
-    }
-
-    public void showDialog(ActionEvent actionEvent) {
         try {
-            Stage stage = new Stage();
-            Parent root = FXMLLoader.load(getClass().getResource("/fxml/add.fxml"));
-            stage.setTitle("Добавление заметки");
-            stage.setHeight(400);
-            stage.setWidth(600);
-            stage.setResizable(false);
-            stage.setScene(new Scene(root));
-            stage.initModality(Modality.WINDOW_MODAL);
-            stage.initOwner(((Node) actionEvent.getSource()).getScene().getWindow());
-            stage.show();
+            fxmlLoader.setLocation(getClass().getResource("/fxml/add.fxml"));
+            fxmlAdd = fxmlLoader.load();
+            addDialogController = fxmlLoader.getController();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void showDialog(ActionEvent actionEvent) {
+        addDialogController.setNote(new Note());
+        if (addDialogStage == null) {
+            addDialogStage = new Stage();
+            addDialogStage.setTitle("Добавление заметки");
+            addDialogStage.setHeight(400);
+            addDialogStage.setWidth(600);
+            addDialogStage.setResizable(false);
+            addDialogStage.setScene(new Scene(fxmlAdd));
+            addDialogStage.initModality(WINDOW_MODAL);
+            addDialogStage.initOwner(mainStage);
+        }
+        addDialogStage.showAndWait();
+        db.add(addDialogController.getNote());
+        db.getUpdateTable();
+    }
+
+    public void setMainStage(Stage mainStage) {
+        this.mainStage = mainStage;
     }
 }
